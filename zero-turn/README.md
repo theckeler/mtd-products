7403fbba690fb079622acfd35e4bc4900051e128
Todd Heckeler <todd@consumedesign.com>
2021-12-14 08:40:57 -0500

    zero turm and ultima updates

---
+++ zero-turn/README.md
@@ -0,0 +1,1 @@
+"npm run start" for webpack dev "npm run build" to build webpack
---
+++ zero-turn/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./stylesheets/ultima.min.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
