b6fa20006aa742b07ef14d6df9d0ec7c84c7109e
Todd Heckeler <todd@consumedesign.com>
2022-07-05 08:49:23 -0400

    updates

---
+++ cc-pro-2-ca/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import Header from "./templates/Header";
+// import Footer from "./templates/Footer";
+// ReactDOM.render(<Header />, document.getElementById("header"));
+// ReactDOM.render(<Footer />, document.getElementById("footer"));
+
+if (module.hot) {
+  module.hot.accept();
+}
